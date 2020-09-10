bb3cad81aaab652df5f76290bfbbb26eef445697
Macmini <macmini@Macminiui-Mac-mini.local>
2020-09-10 16:17:54 +0900

    apng test

--- src/Emoticon.js
+++ src/Emoticon.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import PropTypes from "prop-types";
-
+import ApngComponent from 'react-apng';
 import './App.css';
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function Emoticon({id, imgId, title, drawer}){
     const emoticonSrc = "https://emoticon.emoticbox.com/src/" + imgId;
+    const apngRef = useRef();
+    const onMouseOverEmoticon = () => {
+        const { play } = apngRef.current;
+        play();
+      }
+    
+      const onMouseLeaveEmoticon = () => {
+        const { isPlay, stop } = apngRef.current;
+        if (isPlay) {
+          stop();
+        }
+      };
 
     return (
         <div className="emoticon">
@@ -13,7 +26,14 @@ function Emoticon({id, imgId, title, drawer}){
                 to ={{
                     pathname: `/emoticon/${id}`,
                 }}>
-                <img src={emoticonSrc} alt={title} title={title}/>
+                {/* <img src={emoticonSrc} alt={title} title={title}></ApngComponent> */}
+                <StickerImage
+                    ref={apngRef}
+                    src={emoticonSrc}
+                    alt="Image"
+                    onMouseOver={onMouseOverEmoticon}
+                    onMouseLeave={onMouseLeaveEmoticon}
+                />
                 <h3>{title}</h3>
             </Link>
             <h5>{drawer}</h5>
@@ -26,4 +46,12 @@ Emoticon.propTypes = {
     title: PropTypes.string.isRequired,
     drawer: PropTypes.string.isRequired
 }
+
+const StickerImage = styled(ApngComponent)`
+    width: 158px;
+    height: 158px;
+    vertical-align: middle;
+    object-fit: contain;
+    object-position: center;
+`
 export default Emoticon;
