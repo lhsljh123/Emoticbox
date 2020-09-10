08c79a2c2d612f900b115dd555f5b070cb765a88
Macmini <macmini@Macminiui-Mac-mini.local>
2020-09-10 15:06:58 +0900

    Updates

---
+++ precache-manifest.99bc982a4f1ed1e5afa57bb4f0d59020.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42cf5f3b496e1eeeced162a83b572843",
+    "url": "/Emoticbox/index.html"
+  },
+  {
+    "revision": "1e2ee74c54dc62655cc2",
+    "url": "/Emoticbox/static/css/main.2c7adb8c.chunk.css"
+  },
+  {
+    "revision": "823c98ed52a88b0cd304",
+    "url": "/Emoticbox/static/js/2.9050e5d1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Emoticbox/static/js/2.9050e5d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e2ee74c54dc62655cc2",
+    "url": "/Emoticbox/static/js/main.a35e4d9f.chunk.js"
+  },
+  {
+    "revision": "76de92e21ed2ac93097e",
+    "url": "/Emoticbox/static/js/runtime-main.bf79fbb5.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Emoticbox/precache-manifest.99bc982a4f1ed1e5afa57bb4f0d59020.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Emoticbox/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
